ab5abd42e69258c0e7e0e46fc27c53476e64c40d
the.peter.humphris@gmail.com <Hudsonville1956>
2019-03-19 15:08:36 +1000

    add a password check program

--- sandbox.py
+++ sandbox.py
@@ -1,2 +1,25 @@
 """Peter Humphris"""
 
+MIN_LENGTH = 5
+
+def main():
+    print("Please enter a valid password.")
+    print("A valid passowrd contains at least 5 characters")
+    password = input("> ")
+    while not password_validation(password):
+        print("Invalid password")
+        password = input("> ")
+    censored_password = ""
+    for i in password:
+        censored_password += "*"
+    print("Your " + str(len(password)) + " character password is valid: " + censored_password)
+
+
+def password_validation(password):
+    if len(password) < MIN_LENGTH:
+        print("Password not long enough!")
+        return False
+    else:
+        return True
+
+main()
